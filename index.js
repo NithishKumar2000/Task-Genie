539a2604705068f10a9febd6216f8ff4ce077c84
NithishKumar2000 <rntihish2000@gmail.com>
2021-08-12 12:58:23 +0300

    git commit -m "Almost over"

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const new_card = ({id,image_url,task_title,task_type,task_description}) =>
   <div class="card-header d-flex justify-content-end gap-2">
     
     <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
-    <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>
+    <button type="button" id=${id} class="btn btn-outline-danger" onClick="delete_card.apply(this,arguments)"><i class="fas fa-trash-alt"  id=${id} onClick="delete_card.apply(this,arguments)" ></i></button>
   </div>
   <img src=${image_url} class="card-img-top" alt="...">
   <div class="card-body">
@@ -21,6 +21,40 @@ const new_card = ({id,image_url,task_title,task_type,task_description}) =>
   </div>
 </div>
 </div>`
+
+//To add new card
+const load_task_card= ()=> {
+    const get_initial_data=localStorage.getItem("taskGenie");
+    if(!taskGenie) return;
+
+    const task_objects= JSON.parse(get_initial_data);
+
+    task_objects.forEach((card) => {
+        task_container.insertAdjacentHTML("beforeend",card);
+        global_store.push(card);
+        
+    });
+}
+
+const delete_card= (event) =>{
+  event=window.event;
+  const target_id= event.target.tag_name;
+
+  const new_updated_array= global_store.filter(
+  (card_object)=>card_object.id!==target_id);
+  
+global_store= new_updated_array;
+
+  if(tag_name==="BUTTON"){
+    return event.target.parentNode.parentNode.parentNode.parentNode.removeChild(
+      event.target.parentNode.parentNode.parentNode)
+  }
+
+  return event.target.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
+    event.target.parentNode.parentNode.parentNode.parentNode.parentNode);
+
+};
+
 
 const saveChanges = () => {
     const taskData = { 
